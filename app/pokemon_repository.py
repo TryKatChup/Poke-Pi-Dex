7f865d5a66d623abee5256becb0fb359a7427b38
Michele Righi <righi.michy@gmail.com>
2021-11-04 20:21:43 +0100

    test refactor
    +resize video source
    +pokémon loading with name (classifier label)
    +video source buttons to load pokémon and take a screenshot
    +menu view
    
    to-do:
    disable canvas updating when we don't visualize the video(?)

---
+++ app/pokemon_repository.py
@@ -0,0 +1,75 @@
+import json
+from pokemon import Pokemon
+
+
+class PokemonRepository:
+    def __init__(self, filename):
+        try:
+            f = open(filename, "r", encoding="utf-8")
+            data = json.load(f)
+
+            self.pokemon = {}
+
+            for pkmn in data:
+                # name as key
+                self.pokemon[pkmn["name"]] = Pokemon(pkmn["id"], pkmn["name"], pkmn["type"], pkmn["evolutions"], pkmn["stats"], pkmn["description"])
+                # id as key
+                # self.pokemon[pkmn["id"]] = Pokemon(pkmn["id"], pkmn["name"], pkmn["type"], pkmn["evolutions"], pkmn["stats"], pkmn["description"])
+                # print(self.pokemon[pkmn["id"]].to_string())  # test
+
+            # TEST: if we want to get info about the "to" evolution, we have to loop again since that pokemon is later
+            # on the pokèdex and we hadn't loaded it yet, in the cycle
+            # for pkmn in self.pokemon.values():
+            #     print(pkmn.to_string_evo(self.get_evolutions(pkmn)))
+        except OSError:
+            print("Cannot open file " + filename)
+    '''
+    # test
+    def get_evolutions(self, pkmn):
+        result = ""
+        if pkmn.evolutions["from"] is not None:
+            result += "from: " + self.pokemon[pkmn.evolutions["from"]].name
+        if pkmn.evolutions["to"] is not None:
+            if len(result) > 0:
+                result += ", "
+            # since pokèmon like Eevee can have multiple evolutions, we want to show them all
+            if type(pkmn.evolutions["to"]) is int:
+                result += "to: " + self.pokemon[pkmn.evolutions["to"]].name
+            elif type(pkmn.evolutions["to"]) is list:
+                result += "to: "
+                for i in range(len(pkmn.evolutions["to"])):
+                    result += self.pokemon[pkmn.evolutions["to"][i]].name
+                    if i + 1 != len(pkmn.evolutions["to"]):
+                        result += ", "
+        return result
+    '''
+
+'''
+# Test
+pokemon_repo = PokemonRepository("utilities/first_gen_pokedex.json")
+
+while True:
+    try:
+        # example: get pokèmon by pokèdex id (Squirtle == 7) and print the information
+        pkmn_id = int(input("Pokemon ID: "))
+        if pkmn_id < 1 or pkmn_id > 151:
+            print("ID must be an integer between 1 and 151 inclusive")
+            continue
+        pkmn = pokemon_repo.pokemon[pkmn_id]
+        print(pkmn.to_string_evo(pokemon_repo.get_evolutions(pkmn)))
+    except ValueError:
+        print("ID must be an integer between 1 and 151 inclusive")
+'''
+
+'''
+NB: conversion from JSON obj to python obj
+
+obj             -> dict
+array           -> list
+string          -> str
+null            -> None
+number(int)     -> int
+number(real)    -> float
+true            -> True
+false           -> False
+'''
---
+++ app/video_capture.py
@@ -0,0 +1,37 @@
+import cv2
+
+
+class VideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+        # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (None, None)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+
+'''
+# Class which uses picamera API (?)
+class MyPicameraCapture:
+    def __init__(self):
+        self.camera = PiCamera()
+        self.camera.resolution
+'''
