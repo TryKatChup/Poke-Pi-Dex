2b9c1c225be3e88e5850bdb77aff0105de31731e
Michele Righi <righi.michy@gmail.com>
2021-12-13 15:59:40 +0100

    Update input.py

--- app/input.py
+++ app/input.py
@@ -5,3 +5,17 @@ def register_button(n_gpio, func):
     button = Button(n_gpio)
     button.when_pressed = func
     return button
+
+
+if __name__ == "__main__":
+    from signal import pause
+
+
+    def test_button(n):
+        print("Button " + str(n) + " pressed!")
+
+
+    button12 = register_button(12, lambda: test_button(12))
+    button16 = register_button(16, lambda: test_button(16))
+
+    pause()
