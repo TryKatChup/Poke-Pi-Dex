35b886aa148ce669f663889c005d7d5d18d74fec
Karina <karina.chichifoi@gmail.com>
2021-12-14 16:57:49 +0100

    Refactorone 🔥

--- pokemon-classifier/src/.old/pc.py
+++ pokemon-classifier/src/.old/pc.py
@@ -7,12 +7,13 @@ import numpy as np
 import tensorflow.keras as K
 from typing import Tuple
 
+
 def create_dataset(
-    data_folder: str,
-    epochs: int,
-    batch_size: int, # how many samples are processed in parallel (2^n)
-    res: typing.Tuple[int, int],
-    seed: int,
+        data_folder: str,
+        epochs: int,
+        batch_size: int,  # how many samples are processed in parallel (2^n)
+        res: typing.Tuple[int, int],
+        seed: int,
 ) -> tf.data.Dataset:
     # Import folder with images
     folders = sorted(glob.glob(os.path.join(data_folder, "*", "")))
@@ -31,7 +32,7 @@ def create_dataset(
         label = folder.split(os.path.sep)[-2]
         cur_samples = [(cur_sample, label) for cur_sample in cur_samples]
         cur_samples_len = len(cur_samples)
-        
+
         # Determine train/test/val splits
         cur_test_len = int(test_split * cur_samples_len)
         cur_val_len = int(val_split * cur_samples_len)
@@ -40,7 +41,7 @@ def create_dataset(
         train_samples.extend(cur_samples[:cur_train_len])
         val_samples.extend(cur_samples[:cur_val_len])
         test_samples.extend(cur_samples[:cur_test_len])
-        
+
         # Increment global train/val/test lengths
         train_len += cur_train_len
         val_len += cur_val_len
@@ -60,7 +61,7 @@ def create_dataset(
     train_labels = tf.convert_to_tensor(train_labels)
     # Create Dataset object
     train_dataset = tf.data.Dataset.from_tensor_slices((train_image_paths, train_labels))
-    
+
     # Repeat for validation and test sets
     val_image_paths, val_labels = list(zip(*val_samples))
     val_labels = label_encoder.transform(val_labels)
@@ -80,7 +81,7 @@ def create_dataset(
 
         # some mapping to constant size - be careful with distorting aspec ratios
         image = tf.image.resize(image, res)
-        
+
         # Normalize to [0, 1] range
         image /= 255.
         return image, label
@@ -89,17 +90,17 @@ def create_dataset(
     train_dataset = train_dataset.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
     val_dataset = val_dataset.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
     test_dataset = test_dataset.map(load_image, num_parallel_calls=tf.data.AUTOTUNE)
-    
+
     '''
     # Initial shuffle of the whole dataset with seed
     dataset = dataset.shuffle(buffer_size=data_len, reshuffle_each_iteration=False, seed=seed)
     val_split = test_split = 0.1
-    
+
     test_len = int(test_split * data_len)
     val_len = int(val_split * data_len)
-    
+
     train_len = data_len - val_len - test_len
-    
+
     test_dataset = dataset.take(test_len)
     trainval_dataset = dataset.skip(test_len)
     val_dataset = trainval_dataset.take(val_len)
@@ -125,36 +126,35 @@ def create_dataset(
     # - Change randomly contrast (30%)
     # - Change randomly saturation (20%)
     # - Change randomly hue (only a small percentage for better results)
-    
+
     def random_rotate(image, label):
         # Generate a random number, between 0 and 3 (number of 90 degrees rotation)
         k = tf.random.uniform(shape=(), minval=0, maxval=4, dtype=tf.int32)
         # tf.cond wants always a function :D
         image = tf.image.rot90(image, k)
         return image, label
-    
+
     def random_contrast(image, label):
         k = tf.random.uniform(shape=(), minval=1, maxval=100, dtype=tf.int32)
         seed = (k, 0)
         # tf.cond wants always a function :D
         image = tf.image.stateless_random_contrast(image, lower=0.7, upper=1, seed=seed)
         return image, label
-    
+
     def random_saturation(image, label):
         k = tf.random.uniform(shape=(), minval=1, maxval=100, dtype=tf.int32)
         seed = (k, 0)
         # tf.cond wants always a function :D
         image = tf.image.stateless_random_saturation(image, lower=0.8, upper=1, seed=seed)
         return image, label
-    
+
     def random_hue(image, label):
         k = tf.random.uniform(shape=(), minval=1, maxval=100, dtype=tf.int32)
         seed = (k, 0)
         # tf.cond wants always a function :D
         image = tf.image.stateless_random_hue(image, max_delta=0.01, seed=seed)
         return image, label
-    
-    
+
     # Apply functions on train dataset
     train_dataset = train_dataset.map(
         lambda image, label: (tf.image.random_flip_left_right(image), label),
@@ -162,30 +162,29 @@ def create_dataset(
     ).map(
         random_rotate,
         num_parallel_calls=tf.data.AUTOTUNE
-    #).map(
-    #     random_brightness,
-    #     num_parallel_calls=tf.data.AUTOTUNE
+        # ).map(
+        #     random_brightness,
+        #     num_parallel_calls=tf.data.AUTOTUNE
     ).map(
-         random_contrast,
-         num_parallel_calls=tf.data.AUTOTUNE
+        random_contrast,
+        num_parallel_calls=tf.data.AUTOTUNE
     ).map(
-         random_saturation,
-         num_parallel_calls=tf.data.AUTOTUNE
+        random_saturation,
+        num_parallel_calls=tf.data.AUTOTUNE
     ).map(
-         random_hue,
-         num_parallel_calls=tf.data.AUTOTUNE
+        random_hue,
+        num_parallel_calls=tf.data.AUTOTUNE
     )
 
-    
     train_dataset = train_dataset.shuffle(buffer_size=256, reshuffle_each_iteration=True)
-    
+
     train_dataset = train_dataset.batch(batch_size)
     val_dataset = val_dataset.batch(batch_size)
     test_dataset = test_dataset.batch(1)
-    
+
     train_dataset = train_dataset.repeat(epochs)
     val_dataset = val_dataset.repeat(epochs)
-    
+
     train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
     val_dataset = val_dataset.prefetch(tf.data.AUTOTUNE)
 
@@ -198,35 +197,34 @@ def create_dataset(
         "test_len": test_len,
         "label_encoder": label_encoder
     }
-    
+
     return data_dict
 
 
 def create_model(
-    n_conv: int = 3,
-    use_bn: bool = False,
-    res: typing.Tuple[int, int] = (256, 256)
+        n_conv: int = 3,
+        use_bn: bool = False,
+        res: typing.Tuple[int, int] = (256, 256)
 ):
-    
     # 1. Input
-    # 2. Feature extraction con n_conv livelli e numero crescente di filtri (16 -> 32 -> 64...):
+    # 2. Feature extraction con n_conv livelli e numero crescente di filtri (32 -> 64 -> 128...):
     #     1. Conv2D
     #     2. BatchNorm (se specificato)
     #     3. ReLU
     # 3. Flatten
-    # 4. Classifier con 2 livelli fully connected:
+    # 4. Classifier con 3 livelli fully connected:
     #     1. Dense con 2048 neuroni
     #     2. BatchNorm
     #     3. ReLU
-    #     4. Dense con 151 neuroni (come il numero di classi)
+    #     4. Dense con 150 neuroni (come il numero di classi)
     #     5. Softmax
-    
+
     input_layer = tf.keras.layers.Input(shape=(res[0], res[1], 3))
     x = input_layer
-    
+
     for i in range(n_conv):
         n_filters = 2 ** (i + 4)
- 
+
         # Conv2D + BatchNorm + ReLU
         x = tf.keras.layers.Conv2D(n_filters, 3, padding="same")(x)
         if use_bn:
@@ -234,14 +232,14 @@ def create_model(
         x = tf.keras.layers.ReLU()(x)
 
         x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
-    
+
     flat = tf.keras.layers.Flatten()(x)
     # 1st Dense(1024) + BatchNorm + ReLU
     fc1 = tf.keras.layers.Dense(2048)(flat)
     if use_bn:
         fc1 = tf.keras.layers.BatchNormalization()(fc1)
     fc1 = tf.keras.layers.ReLU()(fc1)
-    
+
     # Dropout
     # fc1 = tf.keras.layers.Dropout(0.5)(fc1)
     # 2nd Dense(150) + softmax
@@ -249,6 +247,127 @@ def create_model(
     fc3 = tf.keras.layers.Softmax()(fc3)
 
     model = tf.keras.Model(inputs=input_layer, outputs=fc3, name="Pokemon-classifier")
-    
+
     return model
+
+
+def top_k_predictions(model, img, label_encoder, k=5):
+    scores = model.predict(img)
+    (top_k_scores, top_k_idx) = tf.math.top_k(scores, k)
+
+    top_k_scores = np.squeeze(top_k_scores.numpy(), axis=0)
+    top_k_idx = np.squeeze(top_k_idx.numpy(), axis=0)
+
+    top_k_labels = label_encoder.inverse_transform(top_k_idx)
+
+    return list(zip(top_k_labels, top_k_scores))
+
+
+def get_label_encoder():
+    encoder = LabelEncoder()
+    encoder.classes_ = np.load('./best_classes.npy')
+    return encoder
+
+
+def res_block(
+        in_layer: K.layers.Layer,
+        filters: int,
+        downscale: bool = False
+) -> K.layers.Layer:
+    # Conv + BN + ReLU
+    x = K.layers.Conv2D(
+        filters,
+        kernel_size=3,
+        padding="same",
+        strides=2 if downscale else 1
+    )(in_layer)
+    x = K.layers.BatchNormalization()(x)
+    x = K.layers.ReLU()(x)
+
+    # Conv + BN
+    x = K.layers.Conv2D(
+        filters,
+        kernel_size=3,
+        padding="same"
+    )(x)
+    x = K.layers.BatchNormalization()(x)
+
+    # Skip connection
+    if downscale:
+        skip = K.layers.Conv2D(
+            filters,
+            kernel_size=1,
+            padding="same",
+            strides=2
+        )(in_layer)
+        skip = K.layers.BatchNormalization()(skip)
+    else:
+        skip = in_layer
+
+    # Add skip
+    x = K.layers.Add()([x, skip])
+
+    # Last ReLU
+    x = K.layers.ReLU()(x)
+
+    return x
+
+
+# %%
+
+def res_stage(
+        in_layer: K.layers.Layer,
+        filters: int,
+        n_res_blocks: int = 2
+) -> K.layers.Layer:
+    # First block which downscales
+    x = res_block(in_layer, filters, downscale=True)
+    # Subsequent blocks
+    for _ in range(n_res_blocks - 1):
+        x = res_block(x, filters)
+
+    return x
+
+
+# %%
+
+def resnet(
+        input_shape: Tuple[int, int],
+        n_stages: int
+) -> K.Model:
+    inputs = K.Input(shape=input_shape)
+
+    # Stem layer
+    x = K.layers.Conv2D(
+        32,
+        kernel_size=3,
+        padding="same",
+        strides=2
+    )(inputs)
+    x = K.layers.Conv2D(
+        32,
+        kernel_size=3,
+        padding="same"
+    )(x)
+    x = K.layers.Conv2D(
+        32,
+        kernel_size=3,
+        padding="same"
+    )(x)
+    x = K.layers.MaxPool2D(
+        pool_size=3,
+        strides=2,
+        padding="same"
+    )(x)
+
+    # ResStages
+    for i in range(n_stages):
+        n_filters = 2 ** (i + 6)
+        x = res_stage(x, n_filters)
+    y = K.layers.GlobalAveragePooling2D()(x)
+    y = K.layers.Dense(151)(y)
+    y = K.layers.Softmax()(y)
+    outputs = y
+
+    return K.Model(inputs, outputs)
 
